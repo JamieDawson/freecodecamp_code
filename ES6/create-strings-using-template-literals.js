157309e05814b6b7cf9f132f01f57263733b490a
JamieDawson <jamierobertdawson@gmail.com>
2020-05-19 21:05:49 -0700

    tempatle literals

---
+++ ES6/create-strings-using-template-literals.js
@@ -0,0 +1,43 @@
+//old code
+/*
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["id-blacklist", "no-dup-keys"]
+};
+function makeList(arr) {
+  "use strict";
+
+  // Only change code below this line
+  const resultDisplayArray = null;
+  // Only change code above this line
+
+  return resultDisplayArray;
+}
+
+const resultDisplayArray = makeList(result.failure);
+*/
+
+//new code
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["id-blacklist", "no-dup-keys"]
+  };
+  function makeList(arr) {
+    "use strict";
+  
+    // Only change code below this line
+  
+    const resultDisplayArray = [];
+    for (let i = 0; i < arr.length; i++) {
+      resultDisplayArray.push(`<li class="text-warning">${arr[i]}</li>`);
+    }
+  
+    // Only change code above this line
+  
+    return resultDisplayArray;
+  }
+  
+  const resultDisplayArray = makeList(result.failure);
+  
