d71a31d43c6396131e37cd01d33091ee728076d6
JamieDawson <jamierobertdawson@gmail.com>
2020-05-19 21:19:55 -0700

    object property shorthand

---
+++ ES6/write-concise-object-literal-declarations-using-object-property-shorthand.js
@@ -0,0 +1,22 @@
+//old code
+/*
+const createPerson = (name, age, gender) => {
+  "use strict";
+  // Only change code below this line
+  return {
+    name: name,
+    age: age,
+    gender: gender
+  };
+  // Only change code above this line
+};
+*/
+
+//new code
+const createPerson = (name, age, gender) => {
+    "use strict";
+    // Only change code below this line
+    return {name, age, gender};
+    // Only change code above this line
+  };
+  
