3d4aada23012201d62d3ee9af4397c4b2a92aba3
JamieDawson <jamierobertdawson@gmail.com>
2020-05-23 16:44:33 -0700

    export

---
+++ ES6/use-export-to-share-a-code-block.js
@@ -0,0 +1,22 @@
+//old code
+/*
+const uppercaseString = (string) => {
+  return string.toUpperCase();
+}
+
+const lowercaseString = (string) => {
+  return string.toLowerCase()
+}
+*/
+
+//new code
+
+const uppercaseString = (string) => {
+    return string.toUpperCase();
+  }
+  
+  const lowercaseString = (string) => {
+    return string.toLowerCase()
+  }
+
+  export {uppercaseString, lowercaseString};
