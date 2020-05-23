e0ae3823e0fb93dc77ad6023da9e424232db633e
JamieDawson <jamierobertdawson@gmail.com>
2020-05-23 13:42:07 -0700

    es6 constructor function

---
+++ ES6/use-class-syntax-to-define-a-constructor-function.js
@@ -0,0 +1,23 @@
+//old code
+/*
+// Only change code below this line
+
+// Only change code above this line
+
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // Should display 'carrot'
+*/
+
+//new code
+
+// Only change code below this line
+class Vegetable {
+    constructor(name){
+      this.name = name;
+    }
+  }
+  // Only change code above this line
+  
+  const carrot = new Vegetable('carrot');
+  console.log(carrot.name); // Should display 'carrot'
+  
