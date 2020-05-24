afd2d67cd9fd58de8f7964fb07c469bb0b8f4644
JamieDawson <jamierobertdawson@gmail.com>
2020-05-23 18:30:11 -0700

    defautl exports

---
+++ ES6/import-a-default-export.js
@@ -0,0 +1,12 @@
+//old code
+/*
+// Only change code above this line
+
+subtract(7,4);
+*/
+
+//new code
+import subtract from './math_functions.js';  
+// Only change code above this line
+
+subtract(7,4);
