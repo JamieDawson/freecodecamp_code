95691a609b4f88d500a36c5d1b09435d6f5171e8
JamieDawson <jamierobertdawson@gmail.com>
2020-05-23 19:00:33 -0700

    rejected promise

---
+++ ES6/handle-a-rejected-promise-with-catch.js
@@ -0,0 +1,38 @@
+//old code
+/*
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer is set to false to represent an unsuccessful response from a server
+  let responseFromServer = false;
+    
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {  
+    reject("Data not received");
+  }
+});
+
+makeServerRequest.then(result => {
+  console.log(result);
+});
+*/
+
+//new code
+
+const makeServerRequest = new Promise((resolve, reject) => {
+    // responseFromServer is set to false to represent an unsuccessful response from a server
+    let responseFromServer = false;
+      
+    if(responseFromServer) {
+      resolve("We got the data");
+    } else {  
+      reject("Data not received");
+    }
+  });
+  
+  makeServerRequest.then(result => {
+    console.log(result);
+  });
+  
+  makeServerRequest.catch(error => {
+    console.log(error);
+  });
