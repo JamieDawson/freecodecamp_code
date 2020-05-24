f832d78505b9db9c95c6ae33d74b6c6140bf5a1e
JamieDawson <jamierobertdawson@gmail.com>
2020-05-23 17:32:49 -0700

    import everything

---
+++ ES6/use-all-to-import-everything-from-file.js
@@ -0,0 +1,14 @@
+//old code
+/*
+// Only change code above this line
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
+*/
+
+//new code
+import * as stringFunctions from './string_functions.js';
+// Only change code above this line
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
